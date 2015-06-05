b29840c6506c5b070ab53905b1f6999adb65b0f0
Administrator <Administrator@PC201401211223>
2015-06-05 21:40:04 +0800

    数据库操作：update 车辆动态信息

--- dao/bikeDao.py
+++ dao/bikeDao.py
@@ -8,12 +8,13 @@ from model.BikeDynamic import BikeDynamicInfo
 
 class BikeDao(IMysqlDao):
     
+    FORMAT_TIME = "%Y-%m-%d %H:%M:%S"
     def __init__(self):
         pass
     
     defaultDynamicSelectSql = "select * from b_bike_dynamic where %s"
     defaultCommonSelectSql = "select * from b_bike_common where %s"
-
+    defautlDyUpdateSql = "update b_bike_dynamic set"
     '''
         动态表操作
     '''
@@ -39,16 +40,71 @@ class BikeDao(IMysqlDao):
         return ret
 
     def setBikeeDyInfoById(self,bikeDynamicInfo):                                                              
-        sql = "update b_bike_dynamic set cur_power = %d ,throttle_state = '%s',brake_state = '%s',motor_state = '%s',lock_state = '%s',indicator_state = '%s',longitude = %f,latitude = %f,speed = %f , time_samp = '%s' where bike_id = %d" % (bikeDynamicInfo["cur_power"],bikeDynamicInfo["throttle_state"],bikeDynamicInfo["brake_state"],bikeDynamicInfo["motor_state"],bikeDynamicInfo["lock_state"],bikeDynamicInfo["indicator_state"],bikeDynamicInfo["longitude"],bikeDynamicInfo["latitude"],bikeDynamicInfo["speed"],bikeDynamicInfo["timesamp"],bikeDynamicInfo["bike_id"])
-        print(sql)
+        sql = "update b_bike_dynamic set cur_power = %d ,throttle_state = %d,brake_state = %d,motor_state = %d,lock_state = %d,indicator_state = %d,longitude = %f,latitude = %f,speed = %f , time_samp = '%s' where bike_id = %d" % (bikeDynamicInfo["cur_power"],bikeDynamicInfo["throttle_state"],bikeDynamicInfo["brake_state"],bikeDynamicInfo["motor_state"],bikeDynamicInfo["lock_state"],bikeDynamicInfo["indicator_state"],bikeDynamicInfo["longitude"],bikeDynamicInfo["latitude"],bikeDynamicInfo["speed"],bikeDynamicInfo["timesamp"],bikeDynamicInfo["bike_id"])
         self.db.execute(sql)
 
     def getBikeDyInfoByid(self,bikeId):
-        cond = "bike_id = %s"
+        cond = "bike_id = %s "
         sql = self.defaultDynamicSelectSql % (cond)
         ret = self.db.get(sql,bikeId)
         return ret
-
+    
+    def setCurPower(self,cur_power,bikeID):
+        cond = "cur_power = %d , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,cur_power,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setThrottleState(self,throttle_state,bikeID):
+        cond = "throttle_state = %d , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,throttle_state,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setBrakeState(self,brake_state,bikeID):
+        cond = "brake_state = %d , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,brake_state,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setMotorState(self,motor_state,bikeID):
+        cond = "motor_state = %d , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,motor_state,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setLockState(self,lock_state,bikeID):
+        cond = "lock_state = %d , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,lock_state,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setIndicatorState(self,indicator_state,bikeID):
+        cond = "indicator_state = %d , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,indicator_state,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setLoLa(self,longitude,latitude,bikeID):
+        cond = "longitude = %f,latitude = %f , time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,longitude,latitude,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
+    def setSpeed(self,speed,bikeID):
+        cond = "speed = %f, time_samp = '%s' "
+        condw = "where  bike_id = %d"
+        temp = self.defautlDyUpdateSql % (cond)
+        sql = temp % (condw)
+        ret = self.db.execute(sql,speed,time.strftime( FORMAT_TIME, time.localtime()),bikeID)
+        return ret
 
     """
         静态表操作
@@ -69,6 +125,7 @@ class BikeDao(IMysqlDao):
         return self.db.query(sql,crutime)
     
 if __name__ == '__main__':
+    
     dao = BikeDao();
     FORMAT_TIME = "%Y-%m-%d %H:%M:%S"
     info = BikeDynamicInfo()
@@ -82,6 +139,6 @@ if __name__ == '__main__':
     info.longitude = 145.3333
     info.latitude = 35.333
     info.speed = 0
-    info.timesamp =  time.strftime( FORMAT_TIME, time.localtime() )
+    info.timesamp =  time.strftime( FORMAT_TIME, time.localtime())
 
     print dao.getRangeeDyByLoLa(116.440255, 39.947385,5000.0)
--- service/bikeService.py
+++ service/bikeService.py
@@ -20,3 +20,30 @@ class BikeService(IService):
         return self.bikeDao.selectCommonAll()
     def selectCommonTimeAll(self,time):
         return self.bikeDao.selectCommonTimeAll(time)
+    
+    def setBikeeDyInfoById(self,bikeDynamicInfo): 
+        self.bikeDao.setBikeeDyInfoById(bikeDynamicInfo)  
+           
+    def setCurPower(self,cur_power,bikeID):
+        self.bikeDao.setCurPower(cur_power,bikeID)
+        
+    def setThrottleState(self,throttle_state,bikeID):
+        self.bikeDao.setThrottleState(throttle_state,bikeID)
+        
+    def setBrakeState(self,brake_state,bikeID):
+        self.bikeDao.setBrakeState(brake_state,bikeID)
+        
+    def setMotorState(self,motor_state,bikeID):
+        self.bikeDao.setMotorState(motor_state,bikeID)
+        
+    def setLockState(self,lock_state,bikeID):
+        self.bikeDao.setLockState(lock_state,bikeID)
+        
+    def setIndicatorState(self,indicator_state,bikeID):
+        self.bikeDao.setIndicatorState(indicator_state,bikeID)
+        
+    def setLoLa(self,longitude,latitude,bikeID):
+        self.bikeDao.setLoLa(longitude,latitude,bikeID)
+    
+    def setSpeed(self,speed,bikeID):
+        self.bikeDao.setSpeed(speed,bikeID)  
